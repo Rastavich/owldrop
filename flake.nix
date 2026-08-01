3c9c2043bdf0199f0382f6e80bb246eab6f4223c
Rastavich <hauserisaac01@gmail.com>
2026-08-01 14:44:30 +1000

    flake: package output (nix run .#tailscale-drop)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "tailscale-drop dev shell (go + nixpkgs' wrapped electron)";
+  description = "tailscale-drop dev shell + NixOS package";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -12,7 +12,24 @@
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [ go electron ];
+        packages = with pkgs; [ go nodejs electron ];
+      };
+
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        pname = "tailscale-drop";
+        version = "0.4.0";
+        src = self;
+        nativeBuildInputs = [ pkgs.go pkgs.makeWrapper ];
+        buildPhase = ''
+          go build -o tailscale-drop .
+        '';
+        installPhase = ''
+          mkdir -p $out/bin $out/lib/tailscale-drop
+          cp tailscale-drop $out/lib/tailscale-drop/
+          cp -r electron $out/lib/tailscale-drop/electron
+          makeWrapper ${pkgs.electron}/bin/electron $out/bin/tailscale-drop \
+            --add-flags "$out/lib/tailscale-drop/electron"
+        '';
       };
     };
 }
